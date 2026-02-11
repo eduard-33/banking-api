37183f37e8c1ee0a7c3a1fa1a5083945abfef936
eduard-33 <205394765+eduard-33@users.noreply.github.com>
2026-02-11 07:21:11 +0400

    Fixed syntax error in urls.py

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-git add config/urls.pyfrom rest_framework_simplejwt.views import (
+from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
